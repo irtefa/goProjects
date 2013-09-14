c2e9238d57968736da51ccfb639e8f512ac855a5
Stephen Lee <yoo929@gmail.com>
2013-09-13 19:48:46 -0500

    Modified client code to support regex on key value pairs

--- logClient/grep_client.go
+++ logClient/grep_client.go
@@ -2,11 +2,12 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"net"
-	"io/ioutil"
 	"bufio"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"strings"
 )
 
 const (
@@ -15,27 +16,59 @@ const (
 
 func main() {
 	ipList := []string{}
-    file, _ := os.Open("masterlist.txt")
-    scanner := bufio.NewScanner(file)
-	
+	file, _ := os.Open("masterlist.txt")
+	scanner := bufio.NewScanner(file)
+
 	//Compile list of ip address from masterlist.txt
-    for scanner.Scan() {
+	for scanner.Scan() {
 		var ip_content = scanner.Text()
 		ip_content = ip_content + ":" + PORT
 		ipList = append(ipList, ip_content)
-    }
-	
-	
-	if len(os.Args) < 2 {
-		fmt.Println("ERROR: No arguments presented")
+	}
+
+	if len(os.Args) < 3 {
+		fmt.Println("ERROR: Not enough arguments presented")
 		os.Exit(1)
 	} else {
 		c := make(chan string)
-		
-		for i := 0; i < len(ipList); i++ {
-			go writeToServer(ipList[i], os.Args[1], c)
+
+		key := os.Args[1]
+		value := os.Args[2]
+
+		// Check ^ on key
+		if strings.HasPrefix(key, "^") {
+			key = key[1:len(key)]
+		} else {
+			key = "[^:]*" + key
 		}
-		
+
+		// Check $ on key
+		if strings.HasSuffix(key, "$") {
+			key = key[0 : len(key)-1]
+		} else {
+			key = key + "[^:]*"
+		}
+
+		// Check ^ on value
+		if strings.HasPrefix(value, "^") {
+			value = value[1:len(value)]
+		} else {
+			value = "[^:]*" + value
+		}
+
+		// Check $ on value
+		if strings.HasSuffix(value, "$") {
+			value = value[0 : len(value)-1]
+		} else {
+			value = value + "[^:]*"
+		}
+
+		serverInput := "^" + key + ":" + value + "$"
+
+		for i := 0; i < len(ipList); i++ {
+			go writeToServer(ipList[i], serverInput, c)
+		}
+
 		for i := 0; i < len(ipList); i++ {
 			serverResult := <-c
 			fmt.Println(serverResult)
@@ -50,30 +83,30 @@ func main() {
  * @param message the message to be sent back to the server
  * @param c the channel for returning server messages
  */
-func writeToServer(ipAddr string, message string, c chan string){
+func writeToServer(ipAddr string, message string, c chan string) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", ipAddr)
 	if err != nil {
 		c <- err.Error()
 		return
 	}
-	
+
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
 		c <- err.Error()
 		return
 	}
-	
+
 	_, err = conn.Write([]byte(message))
 	if err != nil {
 		c <- err.Error()
 		return
 	}
-	
+
 	result, err := ioutil.ReadAll(conn)
 	if err != nil {
 		c <- err.Error()
 		return
 	}
-	
+
 	c <- string(result)
 }
