f0b58e804e8b2fa0206d52180688452716e868d9
Stephen Lee <yoo929@gmail.com>
2013-09-13 22:05:03 -0500

    Added client unit tests and updated some bugs in client program

--- logClient/grep_client.go
+++ logClient/grep_client.go
@@ -11,12 +11,13 @@ import (
 )
 
 const (
-	PORT = "8008"
+	PORT        = "8008"
+	MASTER_LIST = "masterlist.txt"
 )
 
 func main() {
 	ipList := []string{}
-	file, _ := os.Open("masterlist.txt")
+	file, _ := os.Open(MASTER_LIST)
 	scanner := bufio.NewScanner(file)
 
 	//Compile list of ip address from masterlist.txt
@@ -35,46 +36,68 @@ func main() {
 		key := os.Args[1]
 		value := os.Args[2]
 
-		// Check ^ on key
-		if strings.HasPrefix(key, "^") {
-			key = key[1:len(key)]
-		} else {
-			key = "[^:]*" + key
-		}
+		// Write proper regex
+		serverInput := rewriteKeyAndValue(key, value)
 
-		// Check $ on key
-		if strings.HasSuffix(key, "$") {
-			key = key[0 : len(key)-1]
-		} else {
-			key = key + "[^:]*"
-		}
-
-		// Check ^ on value
-		if strings.HasPrefix(value, "^") {
-			value = value[1:len(value)]
-		} else {
-			value = "[^:]*" + value
-		}
-
-		// Check $ on value
-		if strings.HasSuffix(value, "$") {
-			value = value[0 : len(value)-1]
-		} else {
-			value = value + "[^:]*"
-		}
-
-		serverInput := "^" + key + ":" + value + "$"
-
+		// Connect to every server in masterlist.txt
 		for i := 0; i < len(ipList); i++ {
 			go writeToServer(ipList[i], serverInput, c)
 		}
 
+		// Print results from server
 		for i := 0; i < len(ipList); i++ {
 			serverResult := <-c
 			fmt.Println(serverResult)
 			fmt.Println("----------")
 		}
 	}
+}
+
+/*
+ * Writes the key value regex patterns as one regex for the grep
+ * to use
+ * @param key the grep pattern for the key
+ * @param value the grep pattern for the value
+ * @return the single grep pattern to query key-values in log files
+ */
+func rewriteKeyAndValue(key string, value string) string {
+	var newKey string
+	var newValue string
+
+	newKey = key
+	newValue = value
+
+	// Check ^ on key
+	if strings.HasPrefix(newKey, "^") {
+		newKey = newKey[1:len(newKey)]
+	} else {
+		newKey = "[^:]*" + newKey
+	}
+
+	// Check $ on key
+	if strings.HasSuffix(newKey, "$") {
+		newKey = newKey[0 : len(newKey)-1]
+	} else {
+		newKey = newKey + "[^:]*"
+	}
+
+	// Check ^ on value
+	if strings.HasPrefix(newValue, "^") {
+		newValue = newValue[1:len(newValue)]
+	} else {
+		newValue = "[^:]*" + newValue
+	}
+
+	// Check $ on value
+	if strings.HasSuffix(newValue, "$") {
+		newValue = newValue[0 : len(newValue)-1]
+	} else {
+		newValue = newValue + "[^:]*"
+	}
+
+	serverInput := "^" + newKey + ":" + newValue + "$"
+
+	return serverInput
 }
 
 /*
