8443f84c1dd589e625e674c46aa739e85bacc35d
Mohd Irtefa <irtefa1@illinois.edu>
2013-10-29 20:41:42 -0500

    implmented consistent hashing functions

---
+++ udpServer/consistentHashing.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"hash/crc32"
+	"math"
+)
+
+/*
+ * create hash for machine
+ */
+func createHash(machineName string) uint32 {
+	h := crc32.NewIEEE()
+	h.Write([]byte(machineName))
+	v := h.Sum32()
+	fmt.Println(v)
+	return v
+}
+
+/*
+ * find the successor machine and return its name (ipaddress#timestamp)
+ */
+func findSuccessor(selfName string, members map[string]Entry) string {
+	var firstMachineHash uint32 = math.MaxUint32
+	var firstMachineName string
+	var successorName string = "none"
+	//setup hashing
+	h := crc32.NewIEEE()
+	//find machine's hash
+	h.Write([]byte(selfName))
+	successorHash := h.Sum32() //assign itself to successorHash
+	//find the next biggest number if there is none, return 0
+	for key, _ := range members {
+		h.Write([]byte(key))
+		v := h.Sum32()
+		//check for the successor
+		if v > successorHash {
+			successorHash = v
+			successorName = key
+		}
+		//update smallest machine
+		if v < firstMachineHash {
+			firstMachineHash = v
+			firstMachineName = key
+		}
+	}
+
+	//if no bigger machine return the smallest
+	if successorName == "none" {
+		return firstMachineName
+	} else {
+		return successorName
+	}
+}
