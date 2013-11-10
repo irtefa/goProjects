ccf40925567a8fd4736b1df47b44301dc4880f53
Stephen Lee <yoo929@gmail.com>
2013-11-09 21:13:09 -0600

    fixed the consistent hashing

--- udpServer/consistentHashing.go
+++ udpServer/consistentHashing.go
@@ -20,30 +20,36 @@ func createHash(machineName string) uint32 {
 /*
  * find the successor machine and return its name (ipaddress#timestamp) return true if it should be handled locally
  */
-func findSuccessor(k uint32, selfName string, members map[string]Entry) (string, uint32) {
+func findSuccessor(hashedKey uint32, selfName string, members map[string]Entry) (string, uint32) {
 	var firstMachineHash uint32 = math.MaxUint32
 	var firstMachineName string
 	var successorName string = "none"
+	var successorHash uint32
+
 	//setup hashing
 	h := crc32.NewIEEE()
 	//find machine's hash
 	selfIp := strings.Split(selfName, "#")[1]
 	h.Write([]byte(selfIp))
-	successorHash := h.Sum32() //assign itself to successorHash
+	successorHash = h.Sum32() //assign itself to successorHash
+
 	//selfHash := successorHash
 	//find the next biggest number if there is none, return 0
-	for key, _ := range members {
-		memberIp := strings.Split(key, "#")[1]
-		v := createHash(memberIp)
+	for member, _ := range members {
+		memberIp := strings.Split(member, "#")[1]
+		memberHash := createHash(memberIp)
+
 		//check for the successor
-		if v > k {
-			successorHash = v
-			successorName = key
+		if memberHash > hashedKey {
+			if memberHash < successorHash {
+				successorHash = memberHash
+				successorName = member
+			}
 		}
 		//update smallest machine
-		if v < firstMachineHash {
-			firstMachineHash = v
-			firstMachineName = key
+		if memberHash < firstMachineHash {
+			firstMachineHash = memberHash
+			firstMachineName = member
 		}
 	}
 
